42bafb46eb3249f46a643c0fe009266d5530995e
Jean-Nicolas Veigel <art.jnveigel@gmail.com>
2023-07-02 10:32:47 +0200

    :bug: guard against undefined classes

--- src/utils/css.ts
+++ src/utils/css.ts
@@ -1,12 +1,17 @@
+import { add } from "lodash";
+
 type ClassDef = [string, boolean | undefined] | string | [ string ];
 
 export const combineClasses = (...classes: ClassDef[]): string => {
-  return classes.reduce((acc: string, classDef) => {
-    let additionalClass
+  return classes.reduce((acc: string, classDef: ClassDef) => {
+    let additionalClass;
+    if(!classDef) {
+      additionalClass = "";
+    }
     if (typeof classDef === "string") {
       additionalClass = classDef
     }
-    else if (classDef.length === 1) {
+    else if (classDef?.length === 1) {
       additionalClass = classDef[0]
     }
     else {
